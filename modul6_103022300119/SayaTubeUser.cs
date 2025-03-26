d994f17f9d7438f0f17717d858439a97e2a62a7b
QIZIR1 <riziqrizwan2@gmail.com>
2025-03-26 16:35:06 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul6_103022300119/SayaTubeUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022300119
+{
+    class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        public string Username;
+
+        public SayaTubeUser(string nama)
+        {
+            this.Username = nama;
+            uploadedVideos = new List<SayaTubeVideo>();
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int hasil = 0;
+
+            for(int i = 0; i < uploadedVideos.Count; i++)
+            {
+                hasil += uploadedVideos[i].getPlayCount();
+            }
+
+            return hasil;
+        }
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+            uploadedVideos.Add(video);
+        }
+
+        public void PrintAllVideoPlay()
+        {
+            foreach (SayaTubeVideo video in this.uploadedVideos)
+            {
+                video.PrintVideoDetails();
+            }
+        }
+    }
+}
---
+++ modul6_103022300119/SayaTubeVideo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022300119
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string tittle)
+        {
+            Debug.Assert(tittle != null, "Judul video tidak boleh null.");
+            Debug.Assert(tittle.Length <= 200, "Judul video tidak boleh ");
+
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID Video: {id}");
+            Console.WriteLine($"Judul Video: {title}");
+            Console.WriteLine($"Jumlah Play Count: {playCount}");
+        }
+
+        public int getPlayCount()
+        {
+            return playCount;
+        }
+    }
+}
